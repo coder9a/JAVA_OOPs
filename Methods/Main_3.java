d3562e6134c17d63557c0c697b05cedb2b01f52c
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2021-04-11 19:51:18 +0530

    Add files via upload

---
+++ Methods/Main_3.java
@@ -0,0 +1,21 @@
+//// Static method trying to access instance variable
+//class Sample3
+//{
+//    int x;
+//    Sample3(int x1)
+//    {
+//        this.x = x1;
+//    }
+//    static void sum()
+//    {
+//        System.out.println("x = "+x);
+//    }
+//}
+//public class Main_3
+//{
+//    public static void main(String[] args)
+//    {
+//        Sample3 s = new Sample3(12);
+//        s.sum();
+//    }
+//}
