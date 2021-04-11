0950fe3bfa10a34e5e2595537a3ce12042d84753
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2021-04-11 19:51:57 +0530

    Add files via upload

---
+++ Methods/Main_7.java
@@ -0,0 +1,12 @@
+//Static Block vs Static method
+class Main_7
+{
+    static
+    {
+        System.out.println("Static Block");
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Static Method");
+    }
+}
